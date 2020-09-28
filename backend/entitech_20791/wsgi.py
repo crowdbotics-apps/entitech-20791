a34fac5b3da1128c023de82df31636752e09263b
Crowdbotics <team@crowdbotics.com>
2020-09-28 20:30:34 +0000

    Update models

--- backend/entitech_20791/wsgi.py
+++ backend/entitech_20791/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entitech_20791.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entitech_20791.settings")
 
 application = get_wsgi_application()
